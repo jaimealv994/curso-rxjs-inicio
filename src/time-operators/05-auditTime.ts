510b7bbcadc03de8a8115287314db6a28c9498ef
Jaime Alvarez <jaimealv994@gmail.com>
2020-05-06 07:01:04 -0600

    finish auditTime lesson

---
+++ src/time-operators/05-auditTime.ts
@@ -0,0 +1,11 @@
+import {fromEvent} from "rxjs";
+import {auditTime, map, tap} from "rxjs/operators";
+
+const click$ = fromEvent<MouseEvent>(document, 'click');
+
+
+click$.pipe(
+  map(({x}) => ({x})),
+  tap(ev => console.log("tap", ev)),
+  auditTime(2000)
+).subscribe(console.log)
